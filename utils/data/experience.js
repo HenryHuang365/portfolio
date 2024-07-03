abd8de3bd7291202b5242edede503c0ac419c7ad
HenryHuang365 <jinghuang365@gmail.com>
2024-07-04 00:59:44 +1000

    portfolio: update the education, experience and about me section

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'AI Software Engineer',
+    company: "Scisets AI",
+    duration: "(May 2024 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Software Engineer Intern",
+    company: "Fleet Space Technologies",
+    duration: "(Dec 2023 - Feb 2024)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Information Technology Developer & Support",
+    company: "Hakka Food",
+    duration: "(Jun 2023 - Nov 2023)"
   }
 ]
