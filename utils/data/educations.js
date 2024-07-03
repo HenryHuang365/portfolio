1e9a086817fa7845ea43a80f5fe637a612ad8218
Henry Huang <109015205+HenryHuang365@users.noreply.github.com>
2024-07-04 01:08:27 +1000

    portfolio: update the education, experience and about me section (#2) (#3)

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of Computer Science",
+    duration: "2020 Sep - 2024 May",
+    institution: "University of New SouthWales",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "High School Certificate",
+    duration: "2018 Jan - 2019 Sep",
+    institution: "Tempe High School",
   }
 ]
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Henry Huang",
   profile: '/profile.jpg',
   designation: "Software Developer",
-  description: "My name is Henry Huang. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "Hi, I'm Henry. An innovative thinker and quick learner, I excel in TypeScript, ReactJS, AWS Cloud, Java, Python, Rust, SQL, and Git. I thrive in agile development environments where my strong communication and collaboration skills come to the forefront. Passionate about software engineering, I am committed to continuously learning and growing to stay up-to-date with advancing technologies. I'm eager to engage in exciting projects that solve real-world problems and make a tangible impact, driving advancements in technology and improving daily operations.",
   email: 'henryh6283@gmail.com',
   phone: '+61 0426 089 646',
   address: 'Sydney, NSW, Australia',
