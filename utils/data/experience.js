c2051faa779090173b47d2617c5d63b4756a6b78
Jing Huang <jinghuang365@gmail.com>
2025-01-14 00:18:08 +1100

    portfolio: Update the experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'AI Software Engineer',
-    company: "Scisets AI",
-    duration: "(May 2024 - Present)"
+    title: "Software Engineer",
+    company: "GHD",
+    duration: "(Jul 2024 - Present)",
   },
   {
     id: 2,
-    title: "Software Engineer Intern",
-    company: "Fleet Space Technologies",
-    duration: "(Dec 2023 - Feb 2024)"
+    title: "AI Software Engineer",
+    company: "Scisets AI",
+    duration: "(May 2024 - Aug 2024)",
   },
   {
     id: 3,
-    title: "Information Technology Developer & Support",
-    company: "Hakka Food",
-    duration: "(Jun 2023 - Nov 2023)"
-  }
-]
+    title: "Software Engineer Intern",
+    company: "Fleet Space Technologies",
+    duration: "(Dec 2023 - Feb 2024)",
+  },
+];
