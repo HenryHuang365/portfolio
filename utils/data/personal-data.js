be51a2e575105ca570841c3b0d323df56d468ea2
HenryHuang365 <jinghuang365@gmail.com>
2025-03-23 20:47:46 +1100

    update the resume link

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,19 @@
 export const personalData = {
   name: "Henry Huang",
-  profile: '/profile.jpg',
+  profile: "/profile.jpg",
   designation: "Software Developer",
-  description: "Hi, I'm Henry. An innovative thinker and quick learner, I excel in TypeScript, ReactJS, AWS Cloud, Java, Python, Rust, SQL, and Git. I thrive in agile development environments where my strong communication and collaboration skills come to the forefront. Passionate about software engineering, I am committed to continuously learning and growing to stay up-to-date with advancing technologies. I'm eager to engage in exciting projects that solve real-world problems and make a tangible impact, driving advancements in technology and improving daily operations.",
-  email: 'henryh6283@gmail.com',
-  phone: '+61 0426 089 646',
-  address: 'Sydney, NSW, Australia',
-  github: 'https://github.com/HenryHuang365',
-  facebook: 'https://www.facebook.com/',
-  linkedIn: 'https://www.linkedin.com/in/henry-huang365/',
-  twitter: 'https://x.com/HenryHuang6282',
-  stackOverflow: 'https://stackoverflow.com/',
+  description:
+    "Hi, I'm Henry. An innovative thinker and quick learner, I excel in TypeScript, ReactJS, AWS Cloud, Java, Python, Rust, SQL, and Git. I thrive in agile development environments where my strong communication and collaboration skills come to the forefront. Passionate about software engineering, I am committed to continuously learning and growing to stay up-to-date with advancing technologies. I'm eager to engage in exciting projects that solve real-world problems and make a tangible impact, driving advancements in technology and improving daily operations.",
+  email: "henryh6283@gmail.com",
+  phone: "+61 0426 089 646",
+  address: "Sydney, NSW, Australia",
+  github: "https://github.com/HenryHuang365",
+  facebook: "https://www.facebook.com/",
+  linkedIn: "https://www.linkedin.com/in/henry-huang365/",
+  twitter: "https://x.com/HenryHuang6282",
+  stackOverflow: "https://stackoverflow.com/",
   leetcode: "https://leetcode.com/u/jinghuang365/",
   devUsername: "said7388",
-  resume: "https://1drv.ms/b/s!ArG12TN-i3y4jOUVgENTPrZoLoFrqQ?e=6Jtuhp"
-}
+  resume:
+    "https://1drv.ms/b/c/b87c8b7e33d9b5b1/EWbGasthHHJIiI4t4pJMSwgBsZoPsmUFJtIElbCO4y4hcA?e=gtm8Qt",
+};
